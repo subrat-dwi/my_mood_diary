9f9fa59e4cc0fcf1aaa3d35bcc27250573fdccb5
subrat-dwi <chubukov789@gmail.com>
2025-05-28 15:18:29 +0530

    2

--- lib/diary_page.dart
+++ lib/diary_page.dart
@@ -5,14 +5,40 @@ class DiaryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController diaryController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text("tell me more..."),
       ),
       body: Column(
         children: [
-          TextFormField(),
-          TextFormField()
+          TextField(
+            maxLines: null,
+            controller: titleController,
+            onChanged: (enter) {
+              titleController.text == enter;
+            },
+            decoration: const InputDecoration(
+                hintStyle: TextStyle(color: Colors.black),
+                hintText: "About ?",
+                label: Text(
+                  "About ?",
+                  style: TextStyle(color: Colors.black),
+                )),
+          ),
+          TextField(
+            maxLines: null,
+            controller: diaryController,
+            onChanged: (enter) {
+              diaryController.text == enter;
+            },
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintStyle: TextStyle(color: Colors.black),
+              hintText: "How are you feeling....",
+            ),
+          ),
         ],
       ),
     );
