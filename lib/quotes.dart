602fe99e00d38179eae5470f0812e575561e76f8
subrat-dwi <chubukov789@gmail.com>
2025-05-28 16:29:27 +0530

    added quotes page

---
+++ lib/quotes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class QuotesPage extends StatelessWidget {
+  const QuotesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Feel Good!!"),
+      ),
+      body:Center(
+        child: Column(
+          children: [
+            Text("Coming soon....")
+          ],
+        ),
+      ),
+    );
+  }
+}
